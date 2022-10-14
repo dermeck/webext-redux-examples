bf915babca32accbf1add988b1c6d19a56925c3b
dermeck <dermeck@posteo.de>
2022-10-14 10:30:05 +0200

    upgrade react18-rr8 example to react 18 and react-redux 8

--- react18-rr8/src/sidebar/index.tsx
+++ react18-rr8/src/sidebar/index.tsx
@@ -3,14 +3,17 @@ import { render } from "react-dom";
 import Sidebar from "./Sidebar";
 import { Store } from "webext-redux";
 import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
 
 const store = new Store();
 
 store.ready().then(() => {
-  render(
+  const container = document.getElementById("sidebar-root");
+  const root = createRoot(container!);
+
+  root.render(
     <Provider store={store}>
       <Sidebar />
-    </Provider>,
-    document.getElementById("sidebar-root")
+    </Provider>
   );
 });
